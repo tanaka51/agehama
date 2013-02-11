ee8b1e5d95ef0e9ccfa8f2b7d09e2f231cd7fa28
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-02-11 14:55:23 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agehama::Application.config.secret_key_base = 'a71008f5325256f17e347b0ce9d410f2c0159c20f4c4fdabd60ba489ffd21eacf83a56df1402d4b5ac6cb15eaa3616bfc2ca2f805694f8ac03d6aafcbaaf7715'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Agehama::Application.config.session_store :encrypted_cookie_store, key: '_agehama_session'
