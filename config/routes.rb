bb6761b818f5a52a62d63f8c0bcc9fd5a538393b
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-02-21 20:18:20 +0900

    Add welcom controller and set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Agehama::Application.routes.draw do
+  root "welcom#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
