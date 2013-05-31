0e7d595d8527cd63ed17a2be53daa3773ca418a7
Koichi Tanaka <k.tanaka@everyleaf.com>
2013-05-31 10:00:14 +0900

    Update to rails 4.0.0.rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Agehama::Application.config.session_store :encrypted_cookie_store, key: '_agehama_session'
+Agehama::Application.config.session_store :cookie_store, key: '_agehama_session'
